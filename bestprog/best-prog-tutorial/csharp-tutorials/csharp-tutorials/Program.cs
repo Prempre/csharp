b8041f0cd10ace030bc2eff1a6e19bc4ba8eb422
Clinton Bartley <prempre1981@gmail.com>
2022-07-03 08:25:16 -0400

    Create Program.cs
    initial commit

---
+++ bestprog/best-prog-tutorial/csharp-tutorials/csharp-tutorials/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Months mn;
+            Months mn2;
+
+            mn = Months.January;
+            mn2 = mn + 1;
+
+            Console.WriteLine($"{mn} - {mn2}");
+
+            
+        }
+    }
+
+    enum Months
+    {
+        January, February, Murch, April, May, Jun, July, August, September, October, November, December
+    };
+
+}
